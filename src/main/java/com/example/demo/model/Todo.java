42e58e60a769faa0f5b2b1356b4682b016aaf995
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-05 17:52:31 +0530

    Added hashcode and equals

--- src/main/java/com/example/demo/model/Todo.java
+++ src/main/java/com/example/demo/model/Todo.java
@@ -1,3 +1,4 @@
+
 package com.example.demo.model;
 
 import java.util.Date;
@@ -59,6 +60,30 @@ public class Todo {
 				+ isDone + "]";
 	}
 	
-	
+	@Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Todo other = (Todo) obj;
+        if (id != other.id) {
+            return false;
+        }
+        return true;
+    }
 
 }
