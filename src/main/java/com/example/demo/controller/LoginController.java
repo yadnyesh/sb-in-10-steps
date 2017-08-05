e438ba3aa04198507486ea62fc550b65a8b5cd9d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-05 12:01:55 +0530

    Read name parameter

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,13 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
 	@RequestMapping("/login")
-	public String sayHello() {
+	public String loginMessage(@RequestParam String name) {
+		System.out.println("name is: " + name);
 		return "login";
 	}
 
