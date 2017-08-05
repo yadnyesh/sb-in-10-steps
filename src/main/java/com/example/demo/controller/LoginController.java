b849fe88dabe83cf7e13ceb232d35647a90985e3
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-05 11:19:56 +0530

    Using only @Controller with RequestMapping and ResponseBody

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class LoginController {
 	
-	@GetMapping("/login")
+	@RequestMapping("/login")
+	@ResponseBody
 	public String sayHello() {
 		return "Hello World";
 	}
