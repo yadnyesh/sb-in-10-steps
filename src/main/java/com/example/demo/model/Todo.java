357b8aa2ef60da5115c60c90e412612bbcac28e1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-05 13:05:08 +0530

    Changes to Todo model

--- src/main/java/com/example/demo/model/Todo.java
+++ src/main/java/com/example/demo/model/Todo.java
@@ -8,6 +8,18 @@ public class Todo {
 	private String desc;
 	private Date targetDate;
 	private boolean isDone;
+	
+	public Todo() {
+	}
+	
+	public Todo(int id, String user, String desc, Date targetDate, boolean isDone) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.desc = desc;
+		this.targetDate = targetDate;
+		this.isDone = isDone;
+	}
 	public int getId() {
 		return id;
 	}
