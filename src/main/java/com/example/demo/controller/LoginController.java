e2cee262a15743cbe549db3f52b35599deab9a46
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-05 12:35:15 +0530

    Login and Welcome pages different

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -15,9 +16,9 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String showWelcomePage(ModelMap model) {
-		//model.put("name", name);
-		return "login";
+	public String showWelcomePage(ModelMap model, @RequestParam String name) {
+		model.put("name", name);
+		return "welcome";
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+<title>First Web Application</title>
+</head>
+
+<body>
+	Welcome ${name}!!
+</body>
+
+</html>
