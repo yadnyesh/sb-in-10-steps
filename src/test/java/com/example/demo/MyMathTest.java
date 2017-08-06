e96a97924f47825906ff63a1288f6ecc0ace6af6
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-06 09:24:40 +0530

    Annotation - Before

--- src/test/java/com/example/demo/MyMathTest.java
+++ src/test/java/com/example/demo/MyMathTest.java
@@ -2,14 +2,21 @@ package com.example.demo;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.example.demo.forjunit.MyMath;
 
 public class MyMathTest {
+	
+	@Before
+	public void before() {
+		System.out.println("Before");
+	}
 
 	@Test
 	public void sumWithThreeNumbers() {
+		System.out.println("Test1");
 		MyMath myMath = new MyMath();
 		int result = myMath.sum(new int[] {1,2,3});
 		assertEquals(6, result);		
@@ -17,6 +24,7 @@ public class MyMathTest {
 	
 	@Test
 	public void sumWithOneNumberr() {
+		System.out.println("Test2");
 		MyMath myMath = new MyMath();
 		int result = myMath.sum(new int[] {3});
 		assertEquals(3, result);		
