217bd2a31707423c64b3baf2eb7b9f794f6f481d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-06 06:42:04 +0530

    Removing LoginService code

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -9,13 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.example.demo.service.LoginService;
 
 @Controller
 public class LoginController {
-	
-	@Autowired
-	LoginService loginService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showLoginPage(ModelMap model) {
