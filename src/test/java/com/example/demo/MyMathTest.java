b9b513d2afedfd91c186710eb502e95ab3170185
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-06 08:53:36 +0530

    Start writing JUNIT

---
+++ src/test/java/com/example/demo/MyMathTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MyMathTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+}
