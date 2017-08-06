b26dbacf42e0d6bf62d70a3d2e0e4f09aebb954d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-06 17:15:19 +0530

    Creating Service and Controller for Surveys and is related questions

---
+++ src/main/java/com/example/demo/controller/SurveyController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Question;
+import com.example.demo.service.SurveyService;
+
+@RestController
+class SurveyController {
+	@Autowired
+	private SurveyService surveyService;
+
+	@GetMapping("/surveys/{surveyId}/questions")
+	public List<Question> retrieveQuestions(@PathVariable String surveyId) {
+		return surveyService.retrieveQuestions(surveyId);
+	}
+}
