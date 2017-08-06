1d5a5c309f6b0f61fc63334ae9010767270b1a4e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-06 16:46:52 +0530

    Spring Boot Again!

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 	
+	@Autowired
+	WelcomeService welcomeService;
+	
 	@RequestMapping("/welcome")
 	public String welcome() {
-		return "This is great!";
+		return welcomeService.retrieveMessage();
+	}
+	
+	@Component
+	public class WelcomeService {
+		public String retrieveMessage() {
+			return "This is great!";
+		}
 	}
 
 }
