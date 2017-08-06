2d683d78e6b62bd3530279d64917c68ffe132770
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-06 06:32:17 +0530

    extends WebSecurityConfigurerAdapter

--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -1,8 +1,9 @@
 package com.example.demo.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfiguration {
-
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
 }
