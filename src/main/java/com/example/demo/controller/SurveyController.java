6d3a4abb45a98a89f4b056144a301ff7a91eb863
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-06 18:09:10 +0530

    POST Method

--- src/main/java/com/example/demo/controller/SurveyController.java
+++ src/main/java/com/example/demo/controller/SurveyController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.model.Question;
 import com.example.demo.service.SurveyService;
@@ -20,9 +25,26 @@ class SurveyController {
 		return surveyService.retrieveQuestions(surveyId);
 	}
 	
-	 @GetMapping(path = "/surveys/{surveyId}/questions/{questionId}")
+	@GetMapping(path = "/surveys/{surveyId}/questions/{questionId}")
 	    public Question retrieveQuestion(@PathVariable String surveyId,
 	            @PathVariable String questionId) {
 	        return surveyService.retrieveQuestion(surveyId, questionId);
 	    }
+	 
+	@PostMapping("/surveys/{surveyId}/questions")
+	    ResponseEntity<?> add(@PathVariable String surveyId,
+	            @RequestBody Question question) {
+
+	        Question createdTodo = surveyService.addQuestion(surveyId, question);
+
+	        if (createdTodo == null) {
+	            return ResponseEntity.noContent().build();
+	        }
+
+	        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+	                .path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+
+	        return ResponseEntity.created(location).build();
+
+	   } 
 }
