b6ed162563f878a221f0e2e536d581ad89538a15
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-06 11:08:20 +0530

    Mockito

--- src/main/java/com/example/demo/mockitodemo/SomeBusinessImpl.java
+++ src/main/java/com/example/demo/mockitodemo/SomeBusinessImpl.java
@@ -3,8 +3,9 @@ package com.example.demo.mockitodemo;
 public class SomeBusinessImpl {
 	private DataService dataService;
 	
-	public SomeBusinessImpl(DataService dataServicemock) {
-		// TODO Auto-generated constructor stub
+	public SomeBusinessImpl(DataService dataService) {
+		super();
+		this.dataService = dataService;
 	}
 
 	public int findTheGreatestFromAllData() {
--- src/test/java/com/example/demo/mockitodemo/SomeBusinessImplTest.java
+++ src/test/java/com/example/demo/mockitodemo/SomeBusinessImplTest.java
@@ -1,23 +1,28 @@
 package com.example.demo.mockitodemo;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SomeBusinessImplTest {
 
+	@Mock
+	DataService dataServiceMock;
+	
+	@InjectMocks
+	SomeBusinessImpl businessImpl;
+	
 	@Test
-	public void findTheGreatestFromAllData() {
-		
-		DataService dataServicemock = mock(DataService.class);
-		when(dataServicemock.retreiveAllData()).thenReturn(new int[] {25, 15, 6}); 
-		
-		
-		SomeBusinessImpl businessImpl  = new SomeBusinessImpl(dataServicemock);
+	public void findTheGreatestFromAllData() {		
+		when(dataServiceMock.retreiveAllData()).thenReturn(new int[] {25, 15, 6}); 	
 		int result = businessImpl.findTheGreatestFromAllData();
-		assertEquals(24, result);
+		assertEquals(25, result);
 	}
 }
 
