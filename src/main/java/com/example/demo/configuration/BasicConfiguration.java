871dc933a0b19087345c40f552d056d70a73ad91
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-07 06:03:02 +0530

    Adding properties to Configuration

--- src/main/java/com/example/demo/configuration/BasicConfiguration.java
+++ src/main/java/com/example/demo/configuration/BasicConfiguration.java
@@ -1,5 +1,27 @@
 package com.example.demo.configuration;
 
 public class BasicConfiguration {
-
+	private boolean value;
+	private String message;
+	private int number;
+	public boolean isValue() {
+		return value;
+	}
+	public void setValue(boolean value) {
+		this.value = value;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
 }
