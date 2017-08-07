90a3abe8e22a29d3d4b13befee3a20aa700a01a2
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-08-07 06:09:21 +0530

    Look for implementation of Map.

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,16 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.configuration.BasicConfiguration;
+
 @RestController
 public class WelcomeController {
 	
 	@Autowired
 	WelcomeService welcomeService;
+	
+	@Autowired
+	BasicConfiguration basicConfiguration;
 	
 	@Value("${welcome.message}")
 	private String welcomeMessage;
@@ -21,11 +29,20 @@ public class WelcomeController {
 		//return welcomeService.retrieveMessage();
 	}
 	
+	@RequestMapping("/dynamic-configuration")
+	public Map dynamicConfiguration(){
+		Map map = new HashMap();
+		map.put("message", basicConfiguration.getMessage());
+		map.put("number", basicConfiguration.getNumber());
+		map.put("value", basicConfiguration.isValue());
+		return map;
+	}
+	
+	
 	@Component
 	public class WelcomeService {
 		public String retrieveMessage() {
 			return "This is great!";
 		}
 	}
-
 }
